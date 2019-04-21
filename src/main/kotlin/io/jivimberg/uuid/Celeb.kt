c5fc1f49eceab15bd275bfb9b1ad03197541cc16
Juan Vimberg <jvimberg@netflix.com>
2019-04-20 17:50:55 -0700

    Adding equals and hashcode to Celeb

--- src/main/kotlin/io/jivimberg/uuid/Celeb.kt
+++ src/main/kotlin/io/jivimberg/uuid/Celeb.kt
@@ -1,5 +1,6 @@
 package io.jivimberg.uuid
 
+import com.sun.xml.internal.fastinfoset.algorithm.BooleanEncodingAlgorithm
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 import javax.persistence.Column
@@ -10,10 +11,19 @@ import javax.persistence.Version
 @Entity
 class Celeb(
         @Id @Column(name = "id", length = 16, unique = true, nullable = false)
-        val id: UUID? = UUID.randomUUID(),
+        val id: UUID = UUID.randomUUID(),
         @Version
         val version: Long? = null,
         val name: String
-)
+) {
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        javaClass != other?.javaClass -> false
+        id != (other as Celeb).id -> false
+        else -> true
+    }
+
+    override fun hashCode(): Int = id.hashCode()
+}
 
 interface CelebRepository : CrudRepository<Celeb, UUID>
